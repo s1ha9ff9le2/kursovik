8df3d4d0784cf8e4c229734319c97cb3d2dc4f65
Dmytro Kikhtenko <shaffle.shaffle@gmail.com>
2014-05-26 21:17:58 +0400

    first commit

---
+++ Hotel project/Hotel project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hotel project
+//
+//  Created by Admin on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
