264b769150e99d999e263146dd3997bcbd1d6b30
Vitaliy <vitaliy2011d@gmail.com>
2014-05-26 20:31:36 +0300

    MOPED

---
+++ Hotel project/Hotel project/LOLViewController.h
@@ -0,0 +1,13 @@
+//
+//  LOLViewController.h
+//  Hotel project
+//
+//  Created by Vitaliy on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOLViewController : UIViewController
+
+@end
---
+++ Hotel project/Hotel project/LOLViewController.m
@@ -0,0 +1,38 @@
+//
+//  LOLViewController.m
+//  Hotel project
+//
+//  Created by Vitaliy on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import "LOLViewController.h"
+
+@interface LOLViewController ()
+
+@end
+
+@implementation LOLViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
