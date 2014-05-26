9999b7369b739a1187c6800a7d220deaf5d73ffa
Dmytro Kikhtenko <shaffle.shaffle@gmail.com>
2014-05-26 21:43:21 +0400

    26 21:43

---
+++ Hotel project/Hotel project/sdsqTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  sdsqTableViewController.h
+//  Hotel project
+//
+//  Created by Admin on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sdsqTableViewController : UITableViewController
+
+@end
