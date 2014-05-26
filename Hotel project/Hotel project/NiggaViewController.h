acbad60cc433bbf9c51b8c2242b52aec2d613f18
Vitaliy <vitaliy2011d@gmail.com>
2014-05-26 20:47:51 +0300

    new class

---
+++ Hotel project/Hotel project/NiggaViewController.h
@@ -0,0 +1,13 @@
+//
+//  NiggaViewController.h
+//  Hotel project
+//
+//  Created by Vitaliy on 26.05.14.
+//  Copyright (c) 2014 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NiggaViewController : UIViewController
+
+@end
